a37c97ea62c0d44b64ce44ef716f4dd410092083
SeHo07 <61018913+SeHo07@users.noreply.github.com>
2026-07-15 19:10:57 +0900

    Add files via upload

---
+++ build_html.py
@@ -0,0 +1,6 @@
+import json
+REC=json.load(open('records4.json'))
+TPL=open('template_body.html').read()
+body=TPL.replace('__REC__', json.dumps(REC, ensure_ascii=False))
+open('입시결과_학교_전형_학과별.html','w').write(body)
+print('written chars', len(body))
